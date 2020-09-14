9742c84e52db0510879cd544860844538f26c8a1
JaeyoonLeev <35904239+JaeyoonLeev@users.noreply.github.com>
2020-09-14 20:45:45 +0900

    Create Join.cpp

---
+++ Join.cpp
@@ -0,0 +1,12 @@
+#include "Join.h"
+
+//using namespace System;
+//
+//[STAThreadAttribute]
+//
+//int main() {
+//
+//	System::Windows::Forms::Application::Run(gcnew VideoShopManagement::Join());
+//	return 0;
+//
+//}
